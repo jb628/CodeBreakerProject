ebe366db810c8c07d3dedfce2fb39826af84c959
jb628 <jon.buch3@gmail.com>
2018-01-05 21:08:15 -0500

    complete task 5

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,10 @@ function guess() {
 }
 
 //implement new functions here
+function setHiddenFields() {
+  answer.value = Math.floor(Math.random()*10000);
+}
+
+
+/*let f = Math.floor(Math.random()*10000);
+console.log(f); */
