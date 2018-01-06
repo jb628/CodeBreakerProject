b9b3584877999007bded79829cf112241d8f66b3
jb628 <jon.buch3@gmail.com>
2018-01-05 21:30:38 -0500

    complete task 6

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,6 +9,25 @@ function guess() {
 //implement new functions here
 function setHiddenFields() {
   answer.value = Math.floor(Math.random()*10000);
+  stringNumber = answer.value.toString();
+  switch (answer.value) {
+    case answer.value < 10:
+      stringNumber = "000" + stringNumber;
+      answer.value = stringNumber;
+      break;
+    case answer.value < 100:
+      stringNumber = "00" + stringNumber;
+      answer.value = stringNumber;
+      break;
+    case answer.value < 1000:
+      stringNumber = "0" + stringNumber;
+      answer.value = stringNumber;
+      break;
+    default:
+      answer.value = stringNumber;
+  }
+
+
 }
 
 
