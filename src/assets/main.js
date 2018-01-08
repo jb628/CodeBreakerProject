c005bcaa1c7b995e81706fa22f7ca1d0e3a8d1f6
jb628 <jon.buch3@gmail.com>
2018-01-07 21:34:12 -0500

    tweak

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,28 +8,46 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields() {
-  answer.value = Math.floor(Math.random()*10000);
+  answer.value = 7;
   stringNumber = answer.value.toString();
-  switch (answer.value) {
-    case answer.value < 10:
+  if (answer.value < 10) {
+    stringNumber = "000" + stringNumber;
+    answer.value = stringNumber;
+  } else if (answer.value < 100) {
+    stringNumber = "00" + stringNumber;
+    answer.value = stringNumber;
+  } else if (answer.value < 1000) {
+    stringNumber = "0" + stringNumber;
+    answer.value = stringNumber;
+  } else {
+    answer.value = stringNumber;
+  }
+
+
+
+  /*switch (answer.value) {
+    case (answer.value < 10):
+      console.log('low');
       stringNumber = "000" + stringNumber;
       answer.value = stringNumber;
       break;
-    case answer.value < 100:
+    case (answer.value < 100):
+      console.log('med');
       stringNumber = "00" + stringNumber;
       answer.value = stringNumber;
       break;
-    case answer.value < 1000:
+    case (answer.value < 1000):
+      console.log('hi');
       stringNumber = "0" + stringNumber;
       answer.value = stringNumber;
       break;
     default:
+      console.log(stringNumber);
       answer.value = stringNumber;
-  }
-
+  }*/
 
 }
 
-
+setHiddenFields();
 /*let f = Math.floor(Math.random()*10000);
 console.log(f); */
